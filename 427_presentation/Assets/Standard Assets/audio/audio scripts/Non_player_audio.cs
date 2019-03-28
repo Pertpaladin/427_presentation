9873831cbf9d6906eaf0ef0012e2812ceca94c47
Pertpaladin <43618687+Pertpaladin@users.noreply.github.com>
2019-03-28 12:51:20 -0500

    initial

---
+++ 427_presentation/Assets/Standard Assets/audio/audio scripts/Non_player_audio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Non_player_audio : MonoBehaviour {
+    public AudioClip one;
+    public AudioClip two;
+    public AudioClip three;
+    private int count = 0;
+    private int selector = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count == 0)
+        {
+            StartCoroutine("PlaySound");
+            count = 1;
+        }
+    }
+
+    private IEnumerator PlaySound()
+    {
+        selector = Random.Range(0, 2);
+        yield return new WaitForSecondsRealtime(10);
+        if (selector == 0)
+        {
+            AudioSource.PlayClipAtPoint(one, this.transform.position);
+        }
+        else if (selector == 1)
+        {
+            AudioSource.PlayClipAtPoint(two, this.transform.position);
+        }
+        else
+            AudioSource.PlayClipAtPoint(three, this.transform.position);
+        count = 0;
+    }
+}
---
+++ 427_presentation/Assets/Standard Assets/audio/audio scripts/Player_audio.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_audio : MonoBehaviour {
+    public AudioSource MainAudio;
+    public AudioClip one;
+    public AudioClip two;
+    public AudioClip three;
+    public bool canPlay = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("1") == true && canPlay == true)
+        {
+            MainAudio.clip = one;
+            MainAudio.Play();
+            canPlay = false;
+            StartCoroutine(PlayerSound());
+        }
+        if (Input.GetKeyDown("2") == true && canPlay == true)
+        {
+            MainAudio.clip = two;
+            MainAudio.Play();
+            canPlay = false;
+            StartCoroutine(PlayerSound());
+        }
+        if (Input.GetKeyDown("3") == true && canPlay == true)
+        {
+            MainAudio.clip = three;
+            MainAudio.Play();
+            canPlay = false;
+            StartCoroutine(PlayerSound());
+        }
+    }
+
+    private IEnumerator PlayerSound()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        canPlay = true;
+    }
+}
